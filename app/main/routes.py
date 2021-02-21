c0515fec33058f0ed39a6a8725cf01aed92bb175
cocdeshijie <2865509822lol@gmail.com>
2021-02-21 12:14:25 -0800

    wip!!!

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,8 +1,7 @@
-from flask import request, render_template, jsonify
+from flask import render_template
 from flask_login import login_required, current_user
 from . import main
 import os
-import secrets
 
 APP_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../files/')
 
@@ -14,4 +13,8 @@ def index():
 @login_required
 def setting():
     print(current_user.get_role())
-    return render_template('setting.html')
+    return render_template('setting.html', current_user=current_user)
+
+@main.route('/documentation')
+def documentation():
+    return 'yes'
